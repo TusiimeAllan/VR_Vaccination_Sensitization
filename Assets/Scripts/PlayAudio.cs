9205798a9027cbc82c76f9fbcbba5407ff613933
TusiimeAllan <allantusiime@gmail.com>
2022-04-07 16:18:18 +0300

    Kampala Street

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource SoundToPlay;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            SoundToPlay.Play();
+        } 
+    }
+ }
---
+++ Assets/Scripts/Traffic/SpawnThem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnThem : MonoBehaviour
+{
+    public GameObject[] characters;
+    public GameObject[] spawnPoints;
+
+    public bool spawn = false;
+
+    void Start()
+    {
+        SpawnRandomPerson();
+    }
+
+    void Update()
+    {
+        if(spawn == true){
+            SpawnRandomPerson();
+        }
+    }
+
+    public void SpawnRandomPerson()
+    {
+        spawn = false;
+        StartCoroutine(SpawnAfterTime());
+    }
+
+    IEnumerator SpawnAfterTime()
+    {
+        yield return new WaitForSeconds(2);
+        produce();
+        produce();
+        spawn = true;
+    }
+
+    public void produce()
+    {
+        GameObject person = Instantiate(characters[Random.Range(0, characters.Length)], this.transform) as GameObject;
+        person.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+    }
+}
