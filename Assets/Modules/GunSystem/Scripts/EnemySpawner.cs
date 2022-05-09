8cc5e0a8d84f5c0cb355313405dcd25db8ac3ec7
TusiimeAllan <allantusiime@gmail.com>
2022-05-09 19:03:12 +0300

    Adding the Shooting functionality in the inside scene

---
+++ Assets/Modules/GunSystem/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject spawnedEnemy;
+
+    public GameObject[] spawnPoints;
+
+    public int spawnTime;
+
+    public GameObject enemyPrefab;
+
+    public AudioSource backgroundMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("spawnEnemies", spawnTime);
+        backgroundMusic.Play();
+  
+    }
+
+    void spawnEnemies()
+    {// This randomizes the enemy spawn points before they move towards the player
+        Invoke("spawnEnemies", spawnTime);
+        float randomX = Random.Range(-7, 7);
+        float randomY = Random.Range(14, 18);
+        float randomZ = Random.Range(30, 35);
+        spawnedEnemy = Instantiate(enemyPrefab);
+        spawnedEnemy.transform.position = new Vector3(randomX, randomY, randomZ);
+    }
+
+    // void spawnEnemies()
+    // {// This randomizes the enemy spawn points before they move towards the player
+    //     Invoke("spawnEnemies", spawnTime);
+        // float randomX = Random.Range(-7, 7);
+        // float randomY = Random.Range(14, 18);
+        // float randomZ = Random.Range(30, 35);
+    //     spawnedEnemy = Instantiate(enemyPrefab);
+    //     spawnedEnemy.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+    // }
+}
---
+++ Assets/Modules/Shooting/Gun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+public class Gun : MonoBehaviour
+{
+    private Player player = null;
+    public GameObject projectile;
+    public float power;
+    public Transform firepoint;
+
+    public SteamVR_Action_Boolean gunTrigger = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Trigger");
+
+    public SteamVR_Action_Boolean grabGripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
+
+    public bool automatic;
+    public float cooldownTime;
+    float time;
+
+    void Update()
+    {
+        // bool rightHandValid = player.rightHand.currentAttachedObject == null ||
+        //             (player.rightHand.currentAttachedObject != null);
+
+        bool rightHandPressed = gunTrigger.GetState(SteamVR_Input_Sources.RightHand);
+
+        bool rightHandReleased = gunTrigger.GetStateUp(SteamVR_Input_Sources.RightHand);
+
+        bool gunOn = grabGripAction.GetStateDown(SteamVR_Input_Sources.RightHand);
+
+
+        if (time > 0f)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            if (gunOn && rightHandPressed)
+            {
+                time = cooldownTime;
+                GameObject proj = (GameObject)Instantiate(projectile, firepoint.position, firepoint.rotation);
+                proj.GetComponent<Rigidbody>().velocity = firepoint.forward * power;
+            }
+        }
+    }
+}
+}
