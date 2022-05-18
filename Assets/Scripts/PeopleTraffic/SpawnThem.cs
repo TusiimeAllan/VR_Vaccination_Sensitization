133e86556ea1a3711a7c94358d57f00d49bc8f55
TusiimeAllan <allantusiime@gmail.com>
2022-05-18 17:35:11 +0300

    Completing the Car System in the Kampala Scene

---
+++ Assets/Scripts/PeopleTraffic/SpawnThem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnThem : MonoBehaviour
+{
+    public GameObject[] characters;
+    public GameObject[] spawnPoints;
+
+    public bool spawn = false;
+
+    public int NumberOfObjectsToSpawn;
+    private int counter = 0;
+
+    void Start()
+    {
+        SpawnRandomPerson();
+    }
+
+    void Update()
+    {
+        if(spawn == true){
+            SpawnRandomPerson();
+        }
+    }
+
+    public void SpawnRandomPerson()
+    {
+        spawn = false;
+        StartCoroutine(SpawnAfterTime());
+    }
+
+    IEnumerator SpawnAfterTime()
+    {
+        if(counter < NumberOfObjectsToSpawn){
+            yield return new WaitForSeconds(2);
+            produce();
+            produce();
+            spawn = true;
+        }else{
+            spawn = false;
+        }
+        
+    }
+
+    public void produce()
+    {
+        GameObject person = Instantiate(characters[Random.Range(0, characters.Length)], this.transform) as GameObject;
+        person.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+        counter++;
+    }
+}
---
+++ Assets/Scripts/PeopleTraffic/npsController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npsController : MonoBehaviour{
+
+    public NavMeshAgent agent;
+
+    public GameObject PATH;
+    public Transform[] PathPoints;
+
+    public float minDistance = 10;
+
+    public int index = 0;
+
+    void Start(){
+        
+        agent = GetComponent<NavMeshAgent>();
+
+        PathPoints = new Transform[PATH.transform.childCount];
+        for(int i = 0; i < PathPoints.Length; i++){
+            PathPoints[i] = PATH.transform.GetChild(i);
+        }
+    }
+
+    void Update(){
+        roam();
+
+        //This is to fix the issue of the agents from floating after being spawned
+        if (transform.hasChanged)
+        {
+            transform.hasChanged = false;
+        }else{
+            gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            gameObject.GetComponent<NavMeshAgent>().enabled = true;
+        }
+    }
+
+//Replicate this for the next time
+    void roam(){
+        if(Vector3.Distance(transform.position, PathPoints[index].position) < minDistance){
+            if(index >= 0 && index < PathPoints.Length){
+                index += 1;
+            }else{
+                index = 0;
+            }
+
+            if(index == PathPoints.Length){
+                index = 0;
+            }
+        }
+
+
+        agent.SetDestination(PathPoints[index].position);
+    }
+}
