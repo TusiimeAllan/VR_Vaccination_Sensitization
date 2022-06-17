16d3ffa80be39811838857b08491b7a28a57c5bd
TusiimeAllan <allantusiime@gmail.com>
2022-06-17 16:08:31 +0300

    Polishing the Inside Scene

--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -3,32 +3,29 @@ using UnityEngine;
 
 public class Floater : MonoBehaviour
 {
-    public float degreesPerSecond = 15.0f;
-    public float amplitude = 0.5f;
-    public float frequency = 1f;
-
-    // public GameObject Player;
-    // public UnityEngine.AI.NavMeshAgent agent;
- 
-    // Position Storage Variables
-    Vector3 posOffset = new Vector3 ();
-    Vector3 tempPos = new Vector3 ();
- 
-    // Use this for initialization
-    void Start () {
-        // Store the starting position & rotation of the object
-        posOffset = transform.position;
-    }
+    public float floatStrength;
+    public float randomRotationStrength;
+    public float floatingPosition;
      
-    // Update is called once per frame
     void Update () {
-        // Spin object around Y-Axis
-        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
- 
-        // Float up/down with a Sin()
-        tempPos = posOffset;
-        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
- 
-        transform.position = tempPos;
+        transform.GetComponent<Rigidbody>().AddForce(Vector3.up *floatStrength);
+        transform.Rotate(randomRotationStrength,randomRotationStrength,randomRotationStrength);
     }
+
+    void FixedUpdate()
+         {
+     
+             if (transform.position.y < floatingPosition)
+             {
+                 Debug.Log("force being applied upwards to move object up");
+                 transform.GetComponent<Rigidbody>().AddForce(Vector3.up * floatStrength);
+                 transform.Rotate(randomRotationStrength, randomRotationStrength, randomRotationStrength);
+             }
+             if (transform.position.y >= floatingPosition)
+             {
+                 Debug.Log("force applied is less than the gravitational force so that the object comes down. Here mass of object is 2.  ");
+                 transform.GetComponent<Rigidbody>().AddForce(Vector3.up * 11.0f);
+                 transform.Rotate(randomRotationStrength, randomRotationStrength, randomRotationStrength);
+             }
+         }
 }
